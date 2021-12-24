bf058d47200fbe92b67909922e79c366eba448fe
Sortinog <96613791+Sortinog@users.noreply.github.com>
2021-12-24 16:22:30 +0800

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,103 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`2000200001010101010101010101010101010101010101010101010101010101010101010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000d0d0d00000000000000000000000000000000000000000000000001010000000d040d0000000000000000000000000000000000020000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000b00000000000000000000000000000001010000000000000000000000000006070c00000000000000000000000000000101000000000300000000000000000a0809000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101000000000000000000000000000000000000000000000000000000000000010100000000000000000000000000000000000000000000000000000000000001010000000000000000000000000000000000000000000000000000000000000101050505050505050505050505050505050505050505050505050505050505010101010101010101010101010101010101010101010101010101010101010101`, img`
+22222222222222222222222222222222
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2...222........................2
+2...2.2........................2
+2..............................2
+2..............................2
+2..............2...............2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+2..............................2
+22222222222222222222222222222222
+`, [myTiles.transparency16,sprites.builtin.brick,sprites.builtin.field0,sprites.builtin.field1,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile7,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile14,sprites.dungeon.floorDark0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile5":
+            case "tile7":return tile7;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
